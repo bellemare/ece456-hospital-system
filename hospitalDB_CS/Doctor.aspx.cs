6583130d7ff9dbb0d5d3676bbbf1c5bcecb6af1c
sunnypurewal <sunnypurewal@93f12486-15b2-11de-bcda-2bb9902f8029>
2009-03-24 22:01:55 +0000

    gridview added for patients

--- hospitalDB_CS/Doctor.aspx.cs
+++ hospitalDB_CS/Doctor.aspx.cs
@@ -11,10 +11,42 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
 
+using MySql.Data.MySqlClient;
+
 public partial class Doctor : System.Web.UI.Page
 {
+    MySqlConnection conServer;
+    MySqlDataReader reader;
+    MySqlCommand cmd;
     protected void Page_Load(object sender, EventArgs e)
     {
+        conServer = new MySqlConnection();
+        conServer.ConnectionString = "Server='localhost';Database='hospital_G004';Uid=root;Pwd='sasha';";
 
+        conServer.Open();
+        Label1.Text = Request.QueryString["empID"];
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        cmd = new MySqlCommand();
+        cmd.CommandText = "SELECT * FROM patients WHERE Doctor=1";
+        cmd.Connection = conServer;
+
+        reader = cmd.ExecuteReader();
+        Patients.DataSource = reader;
+        Patients.DataBind();
+    }
+
+    protected void Patients_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        Patients.EditIndex = e.NewEditIndex;
+        Button1_Click( null, EventArgs.Empty);
+
+    }
+    protected void Patients_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        Patients.DeleteRow(e.RowIndex);
+        Patients.DataBind();
     }
 }
