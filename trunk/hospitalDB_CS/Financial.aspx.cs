39fdfee3787a0eaebafe3d7ef594e95a6bf8bac1
betastas <betastas@93f12486-15b2-11de-bcda-2bb9902f8029>
2009-03-25 17:32:35 +0000

    Added some code to allow for visit creation in Financial.aspx. Commented out.

--- trunk/hospitalDB_CS/Financial.aspx.cs
+++ trunk/hospitalDB_CS/Financial.aspx.cs
@@ -11,14 +11,25 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
 
+using MySql.Data.MySqlClient;
+
 public partial class Financial : System.Web.UI.Page
 {
+    MySqlConnection conServer;
+    MySqlDataReader reader;
+    MySqlCommand cmd;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        conServer = new MySqlConnection();
+        conServer.ConnectionString = "Server='localhost';Database='hospital_G004';Uid=root;Pwd='root';";
+        conServer.Open();
+        cmd = new MySqlCommand();
+        cmd.Connection = conServer;
     }
     protected void docApptSearch_Click(object sender, EventArgs e)
     {
+        
         int docID = 0;
         docID = Int32.Parse(doctorIDBox.Text);
         int[] startDate = new int[5];
@@ -26,11 +37,11 @@ public partial class Financial : System.Web.UI.Page
         startDate[0] = Int32.Parse(startYearBox.Text);
         if (startDate[0] < 0) startDate[0] = 0;
         startDate[1] = Int32.Parse(startMonthBox.Text);
-        if (startDate[1] < 0) startDate[1] = 0;
+        if (startDate[1] < 1) startDate[1] = 1;
         else if(startDate[1] > 12) startDate[1] = 12;
        
         startDate[2] = Int32.Parse(startDayBox.Text);
-        if (startDate[2] < 0) startDate[2] = 0;
+        if (startDate[2] < 1) startDate[2] = 1;
         else if (startDate[2] > 31) startDate[2] = 31;
 
         startDate[3] = Int32.Parse(startHourBox.Text);
@@ -46,11 +57,11 @@ public partial class Financial : System.Web.UI.Page
         if(endDate[0] < 0) endDate[0] = 0;
 
         endDate[1] = Int32.Parse(endMonthBox.Text);
-        if(endDate[1] < 0) endDate[1] = 0;
+        if(endDate[1] < 1) endDate[1] = 1;
         else if(endDate[1] > 12) endDate[1] = 12;
 
         endDate[2] = Int32.Parse(endDayBox.Text);
-        if(endDate[2] < 0) endDate[4] = 0;
+        if(endDate[2] < 1) endDate[4] = 1;
         else if(endDate[2] > 31) endDate[2] = 31;
 
         endDate[3] = Int32.Parse(endHourBox.Text);
@@ -66,6 +77,32 @@ public partial class Financial : System.Web.UI.Page
     
         //doctorIDLabel.Text = start.ToString();
         //doctorSearchLabel.Text = end.ToString();
+        
+
+        cmd.CommandText = "SELECT * FROM visits WHERE DoctorID=" + docID;
+        reader = cmd.ExecuteReader();
+        apptResultGrid.DataSource = reader;
+        apptResultGrid.DataBind();
+
+        /*
+         * Add New Appointment code:
+          
+
+        
+
+
+                DateTime newDate = new DateTime(2004, 9, 14, 12, 30, 0);
+                DateTime newDate2 = new DateTime(2005, 10, 15, 1, 59, 10);
+                int newVisitID = 435;
+                int newDoctorID = 11;
+                int newPatientID = 85;
+                string newComments = "kid is just too fat";
+
+
+                cmd.CommandText = " INSERT INTO visits (VisitID, PatientID, DoctorID, StartDate, EndDate, Comments) VALUES ( " + newVisitID + ", " + newPatientID + ", " + newDoctorID + ", '" + newDate.ToString("s") + "', '" + newDate2.ToString("s") +"','" + newComments + "' )";
+
+                cmd.ExecuteNonQuery();
+         *  */
 
     }
 }
