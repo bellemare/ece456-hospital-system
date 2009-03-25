40817d769e7a1685973c4e44f3f37b3885a3d055
betastas <betastas@93f12486-15b2-11de-bcda-2bb9902f8029>
2009-03-25 16:26:49 +0000

    Added some financial functionality. Still developing GUI

--- trunk/hospitalDB_CS/Financial.aspx.cs
+++ trunk/hospitalDB_CS/Financial.aspx.cs
@@ -17,4 +17,55 @@ public partial class Financial : System.Web.UI.Page
     {
 
     }
+    protected void docApptSearch_Click(object sender, EventArgs e)
+    {
+        int docID = 0;
+        docID = Int32.Parse(doctorIDBox.Text);
+        int[] startDate = new int[5];
+        int[] endDate = new int[5];
+        startDate[0] = Int32.Parse(startYearBox.Text);
+        if (startDate[0] < 0) startDate[0] = 0;
+        startDate[1] = Int32.Parse(startMonthBox.Text);
+        if (startDate[1] < 0) startDate[1] = 0;
+        else if(startDate[1] > 12) startDate[1] = 12;
+       
+        startDate[2] = Int32.Parse(startDayBox.Text);
+        if (startDate[2] < 0) startDate[2] = 0;
+        else if (startDate[2] > 31) startDate[2] = 31;
+
+        startDate[3] = Int32.Parse(startHourBox.Text);
+        if(startDate[3] < 0) startDate[3] = 0;
+        else if(startDate[3] > 23) startDate[3] = 23;
+
+        startDate[4] = Int32.Parse(startMinuteBox.Text);
+        if(startDate[4] < 0) startDate[4] = 0;
+        else if(startDate[4] > 59) startDate[4] = 59;
+
+
+        endDate[0] = Int32.Parse(endYearBox.Text);
+        if(endDate[0] < 0) endDate[0] = 0;
+
+        endDate[1] = Int32.Parse(endMonthBox.Text);
+        if(endDate[1] < 0) endDate[1] = 0;
+        else if(endDate[1] > 12) endDate[1] = 12;
+
+        endDate[2] = Int32.Parse(endDayBox.Text);
+        if(endDate[2] < 0) endDate[4] = 0;
+        else if(endDate[2] > 31) endDate[2] = 31;
+
+        endDate[3] = Int32.Parse(endHourBox.Text);
+        if(endDate[3] < 0) endDate[3] = 0;
+        else if(endDate[3] > 23) endDate[3] = 23;
+
+        endDate[4] = Int32.Parse(endMinuteBox.Text);
+        if(endDate[4] < 0) endDate[4] = 0;
+        else if(endDate[4] > 59) endDate[4] = 59;
+
+        DateTime end = new DateTime(endDate[0], endDate[1], endDate[2], endDate[3], endDate[4], 0);
+        DateTime start = new DateTime(startDate[0], startDate[1], startDate[2], startDate[3], startDate[4], 0);
+    
+        //doctorIDLabel.Text = start.ToString();
+        //doctorSearchLabel.Text = end.ToString();
+
+    }
 }
