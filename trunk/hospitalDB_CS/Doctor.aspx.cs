8ceed0c990b64006fd0348277749374fa7170986
sunnypurewal <sunnypurewal@93f12486-15b2-11de-bcda-2bb9902f8029>
2009-03-26 13:23:27 +0000

    added audit

--- trunk/hospitalDB_CS/Doctor.aspx.cs
+++ trunk/hospitalDB_CS/Doctor.aspx.cs
@@ -61,8 +61,21 @@ public partial class Doctor : System.Web.UI.Page
     
     protected void Patients_RowUpdating(object sender, GridViewUpdateEventArgs e)
     {
-
+        for (int i = 0; i < e.NewValues.Count; i++)
+        {
+            if (e.NewValues[i].ToString() != e.OldValues[i].ToString())
+            {
+                MySqlConnection con = new MySqlConnection("Server='localhost';Database='hospital_G004';Uid=root;Pwd='sasha';");
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = "INSERT INTO audit (TableChanged, ColumnChanged, OldValue, NewValue, DateChanged) VALUES ('patients', '" + (i+1).ToString() + "', '" + e.OldValues[i].ToString() + "', '" + e.NewValues[i].ToString() + "', '" + DateTime.Now + "' )";
+                cmd.Connection = con;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
     }
+
     protected void btnSearch_Click(object sender, EventArgs e)
     {
         PatientData.SelectCommand = "Select * from Patients where Doctor=" + Request.QueryString["empID"] + " AND ( PatientName LIKE '%" + txtPatient.Text + "%' OR PatientID LIKE '%" + txtPatient.Text + "%' OR SIN LIKE '%" + txtPatient.Text + "%')";
