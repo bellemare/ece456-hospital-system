4b07bde9a35306d89379f35bda20f73eaf42e6ff
sasha.papo <sasha.papo@93f12486-15b2-11de-bcda-2bb9902f8029>
2009-03-26 18:08:42 +0000

    Admin.aspx

--- hospitalDB_CS/Admin.aspx.cs
+++ hospitalDB_CS/Admin.aspx.cs
@@ -11,10 +11,27 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
 
+using MySql.Data.MySqlClient;
+
 public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void btnAddEmp_Click(object sender, EventArgs e)
+    {
+        MySqlConnection con = new MySqlConnection("Server='localhost';Database='hospital_G004';Uid=root;Pwd='sasha';");
+        MySqlCommand cmd = new MySqlCommand();
+        cmd.CommandText = "INSERT INTO employees (EmployeeName, Job, Passwd) VALUES ('"+ txtEmpName.Text + "', '" + jobList.SelectedValue +"', '" + txtEmpPass.Text +"' )";
+        cmd.Connection = con;
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+    }
+    protected void employeeData_Updated(object sender, SqlDataSourceStatusEventArgs e)
+    {
+        empGrid.DataSource = employeeData;
+        empGrid.DataBind();
     }
 }
