3b2a888700f30fc36cc49922bdd6bc1015cab403
sunnypurewal <sunnypurewal@93f12486-15b2-11de-bcda-2bb9902f8029>
2009-03-27 19:50:43 +0000

    Patient page is close to being done, doctors can edit, patients can't, great success

--- hospitalDB_CS/ViewPatient.aspx.cs
+++ hospitalDB_CS/ViewPatient.aspx.cs
@@ -20,20 +20,93 @@ public partial class ViewPatient : System.Web.UI.Page
 
     protected void ViewSurgeries_Click(object sender, EventArgs e)
     {
-        PatientData.SelectCommand = "SELECT * FROM visits INNER JOIN surgeries ON surgeries.VisitID=visits.VisitID WHERE PatientID = " + Request.QueryString["patID"] + " AND DoctorID = " + Request.QueryString["empID"];    
-        PatientView.DataSource = PatientData;
-        PatientView.DataBind();
+        string surgEmp = "SELECT visits.VisitID,visits.StartDate,visits.EndDate,visits.Comments,surgeries.Type,surgeries.Comments FROM visits,surgeries WHERE PatientID = " + Request.QueryString["patID"] + " AND DoctorID = " + Request.QueryString["empID"] + " AND visits.VisitID = surgeries.VisitID";
+        string surgPat = "SELECT visits.VisitID,visits.StartDate,visits.EndDate,visits.Comments,surgeries.Type,surgeries.Comments FROM visits,surgeries WHERE PatientID = " + Request.QueryString["patID"] + " AND visits.VisitID = surgeries.VisitID";
+
+        Session["visits"] = 0;
+        Session["surg"] = 1;
+        Session["pres"] = 0;
+        if (Request.QueryString["empID"] != null)
+        {
+            PatientData.SelectCommand = surgEmp;
+        }
+        else
+        {
+            PatientData.SelectCommand = surgPat;
+        }
     }
+
     protected void ViewPrescriptions_Click(object sender, EventArgs e)
     {
-        PatientData.SelectCommand = "SELECT * FROM visits INNER JOIN prescriptions ON prescriptions.VisitID=visits.VisitID WHERE PatientID = " + Request.QueryString["patID"] + " AND DoctorID = " + Request.QueryString["empID"];
-        PatientView.DataSource = PatientData;
-        PatientView.DataBind();
+        string presEmp = "SELECT visits.VisitID,visits.StartDate,visits.EndDate,visits.Comments,prescriptions.Type,prescriptions.Dosage,prescriptions.Comments FROM visits,prescriptions WHERE PatientID = " + Request.QueryString["patID"] + " AND DoctorID = " + Request.QueryString["empID"] + " AND visits.VisitID = prescriptions.VisitID";
+        string presPat = "SELECT visits.VisitID,visits.StartDate,visits.EndDate,visits.Comments,prescriptions.Type,prescriptions.Dosage,prescriptions.Comments FROM visits,prescriptions WHERE PatientID = " + Request.QueryString["patID"] + " AND visits.VisitID = prescriptions.VisitID";
+
+        Session["visits"] = 0;
+        Session["surg"] = 0;
+        Session["pres"] = 1;
+        if (Request.QueryString["empID"] != null)
+        {
+            PatientData.SelectCommand = presEmp;
+        }
+        else
+        {
+            PatientData.SelectCommand = presPat;
+        }
     }
     protected void ViewVisits_Click(object sender, EventArgs e)
     {
-        PatientData.SelectCommand = "SELECT visits.VisitID, visits.StartDate, visits.EndDate, visits.Comments, diagnosis.Outcome, diagnosis.Comments FROM visits,diagnosis WHERE PatientID = " + Request.QueryString["patID"] + " AND DoctorID = " + Request.QueryString["empID"] + " AND visits.VisitID = diagnosis.VisitID";
-        PatientView.DataSource = PatientData;
-        PatientView.DataBind();
+        Session["visits"] = 1;
+        Session["surg"] = 0;
+        Session["pres"] = 0;
+
+        if (Request.QueryString["empID"] != null)
+        {
+            PatientData.SelectCommand = "SELECT visits.VisitID, visits.StartDate, visits.EndDate, visits.Comments, diagnosis.Outcome, diagnosis.Comments FROM visits,diagnosis WHERE PatientID = " + Request.QueryString["patID"] + " AND DoctorID = " + Request.QueryString["empID"] + " AND visits.VisitID = diagnosis.VisitID";
+            PatientView.AutoGenerateEditButton = true;
+        }
+        else
+        {
+            PatientData.SelectCommand = "SELECT visits.VisitID, visits.StartDate, visits.EndDate, visits.Comments, diagnosis.Outcome, diagnosis.Comments FROM visits,diagnosis WHERE PatientID = " + Request.QueryString["patID"] + " AND visits.VisitID = diagnosis.VisitID";
+            PatientView.AutoGenerateEditButton = false;
+        }
+    }
+    protected void PatientView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        PatientView.EditIndex = -1;
+    }
+    protected void PatientView_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        PatientView.EditIndex = e.NewEditIndex;
+        PatientView.SelectedIndex = e.NewEditIndex;
+        if ((int)Session["visits"] == 1)
+            ViewVisits_Click(null, EventArgs.Empty);
+        else if ((int)Session["surg"] == 1)
+            ViewSurgeries_Click(null, EventArgs.Empty);
+        else if ((int)Session["pres"] == 1)
+            ViewPrescriptions_Click(null, EventArgs.Empty);
+
+    }
+
+    protected void PatientView_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        string oldDate;
+        DateTime newDateTime;
+
+        oldDate = ((TextBox)PatientView.SelectedRow.Cells[2].Controls[0]).Text;
+        newDateTime = Convert.ToDateTime(oldDate);
+        Session["newStartDate"] = newDateTime.ToString("yyyy-MM-dd HH:mm:ss");
+        oldDate = ((TextBox)PatientView.SelectedRow.Cells[3].Controls[0]).Text;
+        newDateTime = Convert.ToDateTime(oldDate);
+        Session["newEndDate"] = newDateTime.ToString("yyyy-MM-dd HH:mm:ss");       
+    }
+
+    protected void PatientView_RowUpdated(object sender, GridViewUpdatedEventArgs e)
+    {
+        if ((int)Session["visits"] == 1)
+            ViewVisits_Click(null, EventArgs.Empty);
+        else if ((int)Session["surg"] == 1)
+            ViewSurgeries_Click(null, EventArgs.Empty);
+        else if ((int)Session["pres"] == 1)
+            ViewPrescriptions_Click(null, EventArgs.Empty);
     }
 }
